049f338598002ea5fc19d2c1a531ab06588a3f35
Aedotris ⛧ <levinhkhang631@gmail.com>
2023-02-16 18:00:00 +0700

    #0 commit for 2023-02-16

--- A/commit.py
+++ A/commit.py
@@ -15,11 +15,10 @@ end = datetime.date(current_year, current_month, current_day)
 res_date = start
 
 while res_date <= end:
-    for i in range(random.randrange(1, 6)):
+    for i in range(random.randrange(1, 100)):
         with open('change-file.txt', 'a') as wf:
             wf.write(f'\n{res_date}')
         os.system(f'git add .')
         os.system(f'git commit --date "{res_date}" -m "#{i} commit for {res_date}"')
     res_date += datetime.timedelta(days=1)
 
-os.system(f'git push')
