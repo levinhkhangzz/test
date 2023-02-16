08df8ea9ccc619041c7fd893c63cf3b4a4def518
Aedotris ⛧ <levinhkhang631@gmail.com>
2023-02-17 00:00:00 +0700

    #0 commit for 2023-02-17T00:00:00.000+07:00

--- A/c.js
+++ A/c.js
@@ -3,20 +3,23 @@ const { execSync } = require('child_process');
 const { DateTime } = require('luxon');
 
 async function generateCommits() {
-    const today = DateTime.local();
-    const lastYear = today.minus({ years: 1 });
-    const startDate = lastYear.startOf('day');
-    const endDate = today.endOf('day');
+    while (true) {
+        const today = DateTime.local();
+        const lastYear = today.minus({ years: 1 });
+        const startDate = lastYear.startOf('day');
+        const endDate = today.endOf('day');
 
-    let currentDate = startDate;
-    while (currentDate <= endDate) {
-        const randomCommitCount = getRandomInt(1, 5); // Generate random integer between 1 and 5
-        for (let i = 0; i < randomCommitCount; i++) {
-            fs.appendFileSync('change-file.txt', `\n${currentDate.toISODate()}`);
-            execSync(`git add .`);
-            execSync(`git commit --date "${currentDate}" -m "#${i} commit for ${currentDate}"`);
+        let currentDate = startDate;
+        while (currentDate <= endDate) {
+            const randomCommitCount = getRandomInt(1, 5); // Generate random integer between 1 and 5
+            for (let i = 0; i < randomCommitCount; i++) {
+                fs.appendFileSync('change-file.txt', `\n${currentDate.toISODate()}`);
+                execSync(`git add .`);
+                execSync(`git commit --date "${currentDate}" -m "#${i} commit for ${currentDate}"`);
+            }
+            currentDate = currentDate.plus({ days: 1 });
         }
-        currentDate = currentDate.plus({ days: 1 });
+        console.log("Commit cycle complete. Restarting..."); // Print a message after each commit cycle
     }
 }
 
