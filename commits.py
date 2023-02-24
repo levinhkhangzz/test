2294682a0bc300d5277bb253bd926093a7b2660d
levinhkhangzz <levinhkhang631@gmail.com>
2023-02-24 23:29:31 +0700

    #0 commit for 2023-02-24

--- commits.py
+++ commits.py
@@ -1,32 +1,30 @@
-import time
-import subprocess
+import datetime
+import os
+import random
 
-def change_between_zeros_and_ones(line):
-    new_line = ""
-    for char in line:
-        if char == "0":
-            new_line += "1"
-        elif char == "1":
-            new_line += "0"
-        else:
-            new_line += char
-    return new_line
+today = datetime.date.today()
 
-filename = 'change-file.txt'
+current_year = today.year
+current_month = today.month
+current_day = today.day
 
-with open(filename, 'r') as file:
-    line = file.readline().strip()
+last_year = current_year - 1
+last_month = today.month
+last_day = current_day - 1  # In case it is a leap year
 
-while True:
-    with open(filename, 'r+') as file:
-        content = file.read()
-        file.seek(0)
-        modified_line = change_between_zeros_and_ones(line)
-        file.write(modified_line)
-        file.truncate()
-        
-    # Thực hiện git add và git commit
-    subprocess.run(["git", "add", "."])
-    subprocess.run(["git", "commit", "-m", "Update file.txt"])
+start = datetime.date(last_year, last_month, last_day)
+end = datetime.date(current_year, current_month, current_day)
+res_date = start
 
-    time.sleep(0.1)  # Cân nhắc thời gian chờ tùy thuộc vào tốc độ yêu cầu của bạn
+with open('change-file.txt', 'r') as f:
+    lines = f.readlines()
+
+if lines:
+    lines[0] = str(res_date) + '\n'
+
+    with open('change-file.txt', 'w') as wf:
+        wf.writelines(lines)
+
+for i in range(random.randrange(1, 6)):
+    os.system(f'git add .')
+    os.system(f'git commit --date "{res_date}" -m "#{i} commit for {res_date}"')
