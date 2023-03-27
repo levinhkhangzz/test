b369e1f0e413694b9078e791273163c53b7ade5f
Aedotris ⛧ <levinhkhang631@gmail.com>
2023-03-27 17:33:14 +0700

    #849 commit for 2023-03-27

--- A/commit.py
+++ A/commit.py
@@ -1,36 +1,38 @@
 import datetime
 import os
 import random
-import time
+import time  # Import thư viện time để sử dụng hàm sleep
 
 try:
     count = 0
     while True: 
-        # Chỉnh sửa ngày tháng bắt đầu và kết thúc ở đây
-        start_date = datetime.date(2023, 1, 1)  # Ngày bắt đầu: 1/1/2023
-        end_date = datetime.date(2024, 2, 17)    # Ngày kết thúc: 17/2/2024
+        today = datetime.date.today()
 
-        # Bốc ngẫu nhiên một ngày từ khoảng từ start_date đến end_date
-        random_date = datetime.date(random.randint(start_date.year, end_date.year), 
-                                    random.randint(1, 12), 
-                                    random.randint(1, 28))  # Giả sử mỗi tháng có tối đa 28 ngày để đảm bảo không có lỗi ngày không tồn tại
+        current_year = today.year
+        current_month = today.month
+        current_day = today.day
 
-        while random_date <= end_date:
+        last_year = current_year - 1 #chỗ này trừ 2 là nó sẽ chạy từ 2 năm trước
+        last_month = today.month
+        last_day = current_day - 1  #chỗ này trừ 1 là nó sẽ chạy từ tháng trước
+# xong rồi anh em tạo chạy file gitpush.py để push code len github để tạo lịch sử commit
+        start = datetime.date(last_year, last_month, last_day)
+        end = datetime.date(current_year, current_month, current_day)
+        res_date = start
+
+        while res_date <= end:
             for i in range(random.randrange(1, 6111111111)):
                 with open('change-file.txt', 'a') as wf:
-                    wf.write(f'\n{random_date}')
+                    wf.write(f'\n{res_date}')
                 os.system(f'git add .')
-                os.system(f'git commit --date "{random_date}" -m "#{i} commit for {random_date}"')
+                os.system(f'git commit --date "{res_date}" -m "#{i} commit for {res_date}"')
                 count += 1
                 if count >= 5000000:
                     raise StopIteration  
+            res_date += datetime.timedelta(days=1)
 
-            # Bốc ngẫu nhiên một ngày mới
-            random_date = datetime.date(random.randint(start_date.year, end_date.year), 
-                                        random.randint(1, 12), 
-                                        random.randint(1, 28))
-
-        time.sleep(1)  # Chạy lại sau 1 giây nếu có lỗi
+       
+        time.sleep(1) # chỗ này là nếu lỗi nó sẽ tự động chạy lại sau 1 giây
 except StopIteration:
     print("Success")
 except KeyboardInterrupt:
