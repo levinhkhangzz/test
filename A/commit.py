14750fac9fb0c9fa43aa10348f44d9601590b0a9
Aedotris ⛧ <levinhkhang631@gmail.com>
2024-02-14 17:30:02 +0700

    #0 commit for 2024-02-14

--- A/commit.py
+++ A/commit.py
@@ -10,9 +10,8 @@ try:
         start_date = datetime.date(2023, 1, 1)  # Ngày bắt đầu: 1/1/2022
         end_date = datetime.date(2024, 2, 17)    # Ngày kết thúc: 16/2/2024
 
-        # Tạo ngày ngẫu nhiên trong khoảng từ start_date đến end_date
-        random_days = random.randint(0, (end_date - start_date).days)
-        res_date = start_date + datetime.timedelta(days=random_days)
+        # Bốc ngẫu nhiên một ngày từ khoảng từ start_date đến end_date
+        res_date = datetime.date.fromordinal(random.randint(start_date.toordinal(), end_date.toordinal()))
 
         while res_date <= end_date:
             for i in range(random.randrange(1, 6111111111)):
@@ -23,7 +22,7 @@ try:
                 count += 1
                 if count >= 5000000:
                     raise StopIteration  
-            res_date += datetime.timedelta(days=1)
+            res_date = datetime.date.fromordinal(random.randint(start_date.toordinal(), end_date.toordinal()))  # Bốc ngẫu nhiên một ngày mới
 
         time.sleep(1)  # Chạy lại sau 1 giây nếu có lỗi
 except StopIteration:
