450ae5a41cf50f1f4082d611d5b35b8e520a9ce7
Aedotris ⛧ <levinhkhang631@gmail.com>
2024-02-14 17:31:38 +0700

    #2565 commit for 2024-02-14

--- A/commit.py
+++ A/commit.py
@@ -7,22 +7,26 @@ try:
     count = 0
     while True: 
         # Chỉnh sửa ngày tháng bắt đầu và kết thúc ở đây
-        start_date = datetime.date(2023, 1, 1)  # Ngày bắt đầu: 1/1/2022
-        end_date = datetime.date(2024, 2, 17)    # Ngày kết thúc: 16/2/2024
+        start_date = datetime.date(2023, 1, 1)  # Ngày bắt đầu: 1/1/2023
+        end_date = datetime.date(2024, 2, 17)    # Ngày kết thúc: 17/2/2024
 
         # Bốc ngẫu nhiên một ngày từ khoảng từ start_date đến end_date
-        res_date = datetime.date.fromordinal(random.randint(start_date.toordinal(), end_date.toordinal()))
+        random_date = datetime.date(random.randint(start_date.year, end_date.year), 
+                                    random.randint(1, 12), 
+                                    random.randint(1, 28))  # Giả sử mỗi tháng có tối đa 28 ngày để đảm bảo không có lỗi ngày không tồn tại
 
-        while res_date <= end_date:
+        while random_date <= end_date:
             for i in range(random.randrange(1, 6111111111)):
                 with open('change-file.txt', 'a') as wf:
-                    wf.write(f'\n{res_date}')
+                    wf.write(f'\n{random_date}')
                 os.system(f'git add .')
-                os.system(f'git commit --date "{res_date}" -m "#{i} commit for {res_date}"')
+                os.system(f'git commit --date "{random_date}" -m "#{i} commit for {random_date}"')
                 count += 1
                 if count >= 5000000:
                     raise StopIteration  
-            res_date = datetime.date.fromordinal(random.randint(start_date.toordinal(), end_date.toordinal()))  # Bốc ngẫu nhiên một ngày mới
+            random_date = datetime.date(random.randint(start_date.year, end_date.year), 
+                                        random.randint(1, 12), 
+                                        random.randint(1, 28))  # Bốc ngẫu nhiên một ngày mới
 
         time.sleep(1)  # Chạy lại sau 1 giây nếu có lỗi
 except StopIteration:
