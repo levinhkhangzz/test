8a1be998010056a463bfd6f56a720107f3e0c258
Aedotris ⛧ <levinhkhang631@gmail.com>
2024-02-17 18:25:23 +0700

    #0 commit for 2024-02-17

--- A/commit.py
+++ A/commit.py
@@ -6,9 +6,9 @@ current_year = today.year
 current_month = today.month
 current_day = today.day
 
-last_year = current_year - 1
+last_year = current_year - 0
 last_month = today.month
-last_day = current_day - 1 # In case it is a leap year
+last_day = current_day - 0 # In case it is a leap year
 
 start = datetime.date(last_year, last_month, last_day)
 end = datetime.date(current_year, current_month, current_day)
